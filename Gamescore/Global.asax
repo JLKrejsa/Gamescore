d830d6f848142a6df3331089aff82c901d31a9c9
Jennifer Krejsa <jlkrejsa@gmail.com>
2017-02-09 11:40:39 -0500

    first commit

---
+++ Gamescore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gamescore.MvcApplication" Language="C#" %>
---
+++ Gamescore/Migrations/201702091539089_AddedTeams.cs
@@ -0,0 +1,16 @@
+namespace Gamescore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedTeams : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
